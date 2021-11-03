40a64a48c7d765eeb442388d7bfab9256f9acf81
Documenter.jl <documenter@juliadocs.github.io>
2021-11-03 17:57:36 +0000

    build based on 9e10eed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyArtifacts","category":"page"},{"location":"#MyArtifacts","page":"Home","title":"MyArtifacts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyArtifacts.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyArtifacts]","category":"page"}]
+}
